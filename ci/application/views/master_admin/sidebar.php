ae88e6799a871e2c1e3081b20e026373ca7c24e4
reihanagam <reihanagam7@gmail.com>
2019-09-22 11:00:36 +0700

    update

--- ci/application/views/master_admin/sidebar.php
+++ ci/application/views/master_admin/sidebar.php
@@ -2,10 +2,16 @@
     <div class="lg:relative lg:sticky">
         <nav id="nav-admin" class="px-6 pt-6 overflow-y-auto text-base sticky?lg:h-screen">
             <div class="mb-10">
-                <a href="javascript:void(0)" class="flex items-center px-2 -mx-2 py-1 hover:text-pink-300 font-medium text-white">
-                <i class="fas fa-key w-8"></i>Permission<i class="w-8 ml-5 fas fa-chevron-right"></i></a>
+                <a id="btn-permissions-admin" href="javascript:void(0)" class="flex items-center px-2 -mx-2 py-1 hover:text-pink-300 font-medium text-white">
+                    <i class="fas fa-key w-8"></i>Permissions
+                    <i id="chevron-right-permissions" class="w-8 ml-5 fas fa-chevron-right"></i>
+                </a>
+                <div id="child-permissions-admin" class="overflow-hidden my-3 bg-pink-600">
+                    <a href="#" class="hover:text-pink-300 font-medium inline-block text-white my-3 mx-4" target="_blank"><i class="fas fa-user w-5"></i>User</a>
+                </div>
                 <a id="btn-settings-admin" href="javascript:void(0)" class="flex items-center px-2 -mx-2 py-1 hover:text-pink-300 font-medium text-white">
-                <i class="fas fa-sliders-h w-8"></i>Settings<i id="chevron-right" class="w-8 ml-5 fas fa-chevron-right"></i>
+                    <i class="fas fa-sliders-h w-8"></i>Settings
+                    <i id="chevron-right-settings" class="w-8 ml-5 fas fa-chevron-right"></i>
                 </a>
                 <div id="child-settings-admin" class="overflow-hidden my-3 bg-pink-600">
                     <a href="<?php echo site_url('admin/privileges') ?>" target="_blank" class="hover:text-pink-300 font-medium inline-block text-white my-3 mx-4">
--- ci/assets/js/admin.js
+++ ci/assets/js/admin.js
@@ -2,21 +2,40 @@ $(function() {
     let svg1 = $("#svg-1");
     let svg2 = $("#svg-2");
     let svg3 = $("#svg-3");
+    let child_permissions_admin = document.getElementById("child-permissions-admin");
     let child_settings_admin = document.getElementById("child-settings-admin");
-    let chevron_right = $("#chevron-right");
+    let chevron_right_permissions = $("#chevron-right-permissions");
+    let chevron_right_settings = $("#chevron-right-settings");
+
+    console.log(chevron_right_permissions);
 
     $(document).on("click", "#btn-settings-admin", function() {
         if(child_settings_admin.style.maxHeight)
         {
-            chevron_right.removeClass("fa-chevron-down");
-            chevron_right.addClass("fa-chevron-right");
+            chevron_right_settings.removeClass("fa-chevron-down");
+            chevron_right_settings.addClass("fa-chevron-right");
             child_settings_admin.style.maxHeight = null;
         }
         else
         {
-            chevron_right.removeClass("fa-chevron-right");
-            chevron_right.addClass("fa-chevron-down");
+            chevron_right_settings.removeClass("fa-chevron-right");
+            chevron_right_settings.addClass("fa-chevron-down");
             child_settings_admin.style.maxHeight =  child_settings_admin.scrollHeight + "px";
+        }
+    });
+
+    $(document).on("click", "#btn-permissions-admin", function() {
+        if(child_permissions_admin.style.maxHeight)
+        {
+            chevron_right_permissions.removeClass("fa-chevron-down");
+            chevron_right_permissions.addClass("fa-chevron-right");
+            child_permissions_admin.style.maxHeight = null;
+        }
+        else
+        {
+            chevron_right_permissions.removeClass("fa-chevron-right");
+            chevron_right_permissions.addClass("fa-chevron-down");
+            child_permissions_admin.style.maxHeight =  child_permissions_admin.scrollHeight + "px";
         }
     });
 
