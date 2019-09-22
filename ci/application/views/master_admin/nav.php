55c97ddf9ba92f093b7fe48c4e418caf59a61bb0
reihanagam <reihanagam7@gmail.com>
2019-09-22 20:30:07 +0700

    update

--- ci/application/views/master_admin/nav.php
+++ ci/application/views/master_admin/nav.php
@@ -60,8 +60,15 @@
 
 
     <div id="child-nav-admin" class="absolute overflow-hidden bg-pink-500">
-        <a href="<?php echo site_url('admin/settings/privileges') ?>" target="_blank" class="hover:text-pink-300 font-medium inline-block text-white my-3 mx-4">
-            test
-        </a>
+        <div class="block">
+            <a href="<?php echo site_url('admin/settings/privileges') ?>" target="_blank" class="hover:text-pink-300 font-medium inline-block text-white my-3 mx-4">
+                <i class="fas fa-user w-5"></i> User
+            </a>
+        </div>
+        <div class="block">
+            <a href="javascript:void(0)" target="_blank" class="hover:text-pink-300 font-medium inline-block text-white my-3 mx-4">
+                <i class="fas fa-logout w-5"></i> Logout
+            </a>
+        </div>
     </div>
 </div>
--- ci/application/views/master_admin/sidebar.php
+++ ci/application/views/master_admin/sidebar.php
@@ -6,19 +6,31 @@
                 <i id="chevron-right-permissions" class="ml-5 fas fa-chevron-right"></i>
             </a>
             <div id="child-permissions-admin" class="overflow-hidden my-3 bg-pink-600">
-                <a href="<?php echo site_url('admin/permissions/user') ?>" class="hover:text-pink-300 font-medium inline-block text-white my-3 mx-4" target="_blank"><i class="fas fa-user w-5"></i>User</a>
+                <div class="block">
+                    <a href="<?php echo site_url('admin/permissions/user') ?>" class="hover:text-pink-300 font-medium inline-block text-white my-3 mx-4" target="_blank"><i class="fas fa-user w-5"></i>User</a>
+                </div>
+
+                <div class="block">
+                    <a href="<?php echo site_url('admin/settings/change-password') ?>" target="_blank" class="hover:text-pink-300 font-medium inline-block text-white my-3 mx-4">
+                    <i class="fas fa-sticky-note w-5"></i> Post
+                    </a>
+                </div>
             </div>
-            <a id="btn-settings-admin" href="javascript:void(0)" class="inline-block items-center px-2 -mx-2 py-1 hover:text-pink-300 font-medium text-white">
+            <a id="btn-settings-admin" href="javascript:void(0)" class="inline-block px-2 -mx-2 py-1 hover:text-pink-300 font-medium text-white">
                 <i class="fas fa-sliders-h w-8"></i>Settings
                 <i id="chevron-right-settings" class="ml-5 fas fa-chevron-right"></i>
             </a>
             <div id="child-settings-admin" class="overflow-hidden my-3 bg-pink-600">
-                <a href="<?php echo site_url('admin/settings/privileges') ?>" target="_blank" class="hover:text-pink-300 font-medium inline-block text-white my-3 mx-4">
-                    <i class="fas fa-user-secret w-5"></i>Privileges
-                </a>
-                <a href="<?php echo site_url('admin/settings/change-password') ?>" target="_blank" class="hover:text-pink-300 font-medium inline-block text-white my-3 mx-4">
-                    <i class="fas fa-lock w-5"></i>Change Password
-                </a>
+                <div class="block">
+                    <a href="<?php echo site_url('admin/settings/privileges') ?>" target="_blank" class="hover:text-pink-300 font-medium inline-block text-white my-3 mx-4">
+                        <i class="fas fa-user-secret w-5"></i>Privileges
+                    </a>
+                </div>
+                <div class="block">
+                    <a href="<?php echo site_url('admin/settings/change-password') ?>" target="_blank" class="hover:text-pink-300 font-medium inline-block text-white my-3 mx-4">
+                        <i class="fas fa-lock w-5"></i>Change Password
+                    </a>
+                </div>
             </div>
             <a href="<?php echo site_url('/') ?>" target="_blank" class="inline-block items-center px-2 -mx-2 py-1 hover:text-pink-300 font-medium text-white">
                 <i class="fas fa-home w-8"></i>Back to Front Page
