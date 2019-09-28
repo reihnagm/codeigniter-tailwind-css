47a17aebbce2969ca7b4fa3a8f15beb3ad30f424
reihanagam <reihanagam7@gmail.com>
2019-09-28 11:05:27 +0700

    update

--- ci/application/helpers/method_helper.php
+++ ci/application/helpers/method_helper.php
@@ -5,6 +5,7 @@ function components($view, $data = [])
     require_once APPPATH.'views/'.$view.'.php';
 }
 
+// DEBUGGING ERROR
 function dd($data)
 {
     echo '<pre>';
@@ -17,37 +18,35 @@ function get_menus_admin()
     $CI =& get_instance();
     $CI->load->database();
 
+    $Ci->db->distinct();
     $CI->db->select("a.name parent_name, a.icon");
     $CI->db->from("tbl_app_admin_menu_group a");
-    $CI->db->free_result();
-    $menus = $CI->db->get()->result();
+    $menus_parent = $CI->db->get()->result();
 
     $temp = '';
     $index_parent = 0;
 
-    foreach ($menus as $menu):
+    foreach ($menus_parent as $menu_parent):
         $index_parent++;
         $temp .=    '<div class="block">
                         <a id="btn-admin-dropdown-'.$index_parent.'" href="javascript:void(0)" class="px-2 -mx-2 py-1 inline-block hover:text-pink-300 font-medium text-white">
-                            <i class="'.$menu->icon.' w-8"></i> '.ucfirst($menu->parent_name).'
+                            <i class="'.$menu_parent->icon.' w-8"></i> '.ucfirst($menu_parent->parent_name).'
                             <i class="ml-5 fas fa-chevron-right"></i>
                         </a>
                     </div>';
+
+        // $CI->db->select("b.name child_name");
+        // $CI->db->from("tbl_app_admin_menu b");
+        // $menus_child = $CI->db->get()->result();
+        //
+        // $index_child = 0;
+        // foreach ($menus_child as $menu_parent):
+        //     $index_child++;
+        //     $temp .= '<div>test</div>';
+        // endforeach;
+
+
     endforeach;
 
     return $temp;
 }
-
-
-
-// function get_menus_admin()
-// {
-//     $template = "";
-//
-//     foreach ($variable as $value)
-//     {
-//         $template .= ''
-//     }
-//
-//     return $template;
-// }
