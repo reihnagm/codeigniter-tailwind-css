ef8765a69d8916ab2fa632abf506e5caeb32118e
reihanagam <reihanagam7@gmail.com>
2019-09-30 11:25:10 +0700

    update

--- ci/application/controllers/admin/AdminController.php
+++ ci/application/controllers/admin/AdminController.php
@@ -157,6 +157,7 @@ class AdminController extends Master_Controller
 					</div>
 
 					<div class="flex items-center">
+
 						<div class="w-1/2 px-3">
 							<div class="mb-4">
 							 	<label class="block text-gray-700 text-sm font-bold mb-2" for="'.$key[0].'">
@@ -199,8 +200,16 @@ class AdminController extends Master_Controller
 							 	<label class="block text-gray-700 text-sm font-bold mb-2" for="'.$key[5].'">
 							   		'.$key[5].'
 							 	</label>
-							 	<input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="'.$key[5].'" type="text" placeholder="'.$key[5].'" value="'.$user['gender'].'">
-						   	</div>
+								<div class="relative">
+						        <select class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state">
+						          <option>New Mexico</option>
+						          <option>Missouri</option>
+						          <option>Texas</option>
+						        </select>
+						        <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+						          <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
+						        </div>
+							</div>
 
 							<div class="mb-4">
 							 	<label class="block text-gray-700 text-sm font-bold mb-2" for="'.$key[6].'">
@@ -215,6 +224,8 @@ class AdminController extends Master_Controller
 							 	</label>
 							 	<input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="'.$key[7].'" type="text" placeholder="'.$key[7].'" value="'.$user['created_at'].'">
 						   	</div>
+						</div>
+
 						</div>
 					</div>
 
