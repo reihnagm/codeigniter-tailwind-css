90ee7103a9f8b5042eaa402593b608b48a945490
reihanagam <reihanagam7@gmail.com>
2019-10-15 22:18:32 +0700

    update

--- ci/application/views/auth/sign_up_page.php
+++ ci/application/views/auth/sign_up_page.php
@@ -2,42 +2,43 @@
     <div class="w-1/3 overflow-hidden">
 
         <form id="form-sign-up" class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" data-parsley-validate data-parsley-focus="first">
-            <div class="mb-4">
+
+            <div class="mb-4 form-group">
                 <label class="block text-gray-700 text-sm font-bold mb-2" for="first_name">
                     First Name
                 </label>
                 <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" name="first_name" id="first_name" type="text" placeholder="First Name" data-parsley-required data-parsley-group="block1">
             </div>
 
-            <div class="mb-4">
+            <div class="mb-4 form-group">
                 <label class="block text-gray-700 text-sm font-bold mb-2" for="last_name">
                     Last Name
                 </label>
                 <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" name="last_name" id="last_name" type="text" placeholder="Last Name" data-parsley-required data-parsley-group="block1">
             </div>
 
-            <div class="mb-4">
+            <div class="mb-4 form-group">
                 <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
                     Username
                 </label>
                 <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" name="username" id="username" type="text" placeholder="Username" data-parsley-required data-parsley-username-regex data-parsley-group="block1">
             </div>
 
-            <div class="mb-4">
+            <div class="mb-4 form-group">
                 <label class="block text-gray-700 text-sm font-bold mb-2" for="email">
                     Email
                 </label>
                 <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" name="email" id="email" placeholder="E-mail" data-parsley-required data-parsley-email-regex data-parsley-group="block1">
             </div>
 
-            <div class="mb-4">
+            <div class="mb-4 form-group">
                 <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
                     Password
                 </label>
                 <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password-sign-up-main" type="password" name="password" placeholder="******************" data-parsley-required data-parsley-password-regex data-parsley-group="block1">
             </div>
 
-            <div class="mb-4">
+            <div class="mb-4 form-group">
                 <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
                     Password Confirmation
                 </label>
--- ci/assets/js/auth.js
+++ ci/assets/js/auth.js
@@ -34,20 +34,43 @@ var site_url = $("[name=site_url]").val();
             }
         });
 
+        $("#form-sign-up").parsley({
+            classHandler: function (el) {
+                return el.$element.closest('.form-group');
+            },
+            errorsWrapper: '<div class="form-field invisible opacity-0 border mt-2 text-sm font-bold border-red-400 rounded bg-red-100 px-4 py-3 text-red-700"></div>',
+            errorTemplate: '<p></p>',
+        }).on('field:success', function() {
+            let el = this.$element[0].id
+            $('#'+el).addClass("border-green-500");
+            $('#'+el).parent().find(".form-field").hide(800);
+            $('#'+el).parent().find(".form-field").addClass("opacity-0");
+            $('#'+el).parent().find(".form-field").addClass("invisible");
+        }).on('field:error', function() {
+            let el = this.$element[0].id
+            $('#'+el).parent().find(".form-field").removeClass("invisible");
+            $('#'+el).parent().find(".form-field").addClass("visible");
+            $('#'+el).parent().find(".form-field").show(800);
+            $('#'+el).parent().find(".form-field").animate({opacity: 1.0}, 800);
+            $('#'+el).removeClass("border-green-500");
+        });
+
         $("#form-sign-up").on('submit', function(e){
             e.preventDefault();
 
-            $(this).parsley().validate();
+            // $(this).parsley().validate();
 
             if ($(this).parsley().isValid())
             {
+                $("#form-submit-sign-up").text('');
+                $("#form-submit-sign-up").append('<img src="'+site_url+'assets/loader/loader.gif" style="width: 25px; display: block; margin: 0 auto;">');
+                $("#form-submit-sign-up").addClass("cursor-not-allowed");
+                $("#form-submit-sign-up").addClass("opacity-50");
+                $("#form-submit-sign-up").removeClass("hover:bg-blue-700");
+                $("#form-submit-sign-up").prop("disabled", true);
+
                 $.post(site_url + "sign-up", $("#form-sign-up").serialize(), function(data) {
-                    $("#form-submit-sign-up").text('');
-                    $("#form-submit-sign-up").append('<img src="'+site_url+'assets/loader/loader.gif" style="width: 25px; display: block; margin: 0 auto;">');
-                    $("#form-submit-sign-up").addClass("cursor-not-allowed");
-                    $("#form-submit-sign-up").addClass("opacity-50");
-                    $("#form-submit-sign-up").removeClass("hover:bg-blue-700");
-                    $("#form-submit-sign-up").prop("disabled", true);
+
                 });
             }
        });
