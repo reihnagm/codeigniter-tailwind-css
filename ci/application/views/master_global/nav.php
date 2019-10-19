48f803dfcee7840d93db5b831bb8b027a646fae1
reihanagam <reihanagam7@gmail.com>
2019-10-19 23:23:29 +0700

    update

--- ci/application/views/master_global/nav.php
+++ ci/application/views/master_global/nav.php
@@ -1,23 +1,23 @@
-<nav class="flex overflow-hidden items-center justify-between bg-pink-500 p-6">
+<nav class="flex overflow-hidden items-center justify-between p-6" style="background-image: url(<?php echo base_url('assets/nav.png'); ?>); background-repeat: no-repeat;">
     <div class="flex overflow-hidden items-center flex-shrink-0 text-white mr-6">
-        <span class="font-semibold text-xl tracking-tight">Codeigniter - Tailwind CSS</span>
+        <span class="font-semibold text-xl tracking-tight bg-pink-400 p-2 rounded-full">Codeigniter - Tailwind CSS</span>
     </div>
 
     <div class="w-full flex overflow-hidden flex-grow justify-end items-center w-auto">
         <?php if(isset($_SESSION['logged_in'])): ?>
-            <a href="<?php echo site_url() ?>user/<?php echo $_SESSION['logged_in']['username']. $_SESSION['logged_in']['id']; ?>/profile" class="block text-white hover:text-white mr-4">
+            <a href="<?php echo site_url() ?>user/<?php echo $_SESSION['logged_in']['username']. $_SESSION['logged_in']['id']; ?>/profile" class="block bg-pink-400 rounded-full p-2 text-white hover:bg-pink-500 mr-4">
                 Profile
             </a>
         <?php else: ?>
-            <a href="<?php echo site_url() ?>" class="block text-white hover:text-white mr-4">
+            <a href="<?php echo site_url() ?>" class="block bg-pink-400 rounded-full p-2 text-white hover:bg-pink-500 mr-4">
                 Sign In
             </a>
-            <a href="<?php echo site_url() ?>sign-up-page" class="block text-white hover:text-white mr-4">
+            <a href="<?php echo site_url() ?>sign-up-page" class="block bg-pink-400 rounded-full p-2 text-white hover:bg-pink-500 mr-4">
                 Sign Up
             </a>
         <?php endif; ?>
         <?php if(isset($_SESSION['logged_in'])): ?>
-            <a id="sign-out" href="javascript:void(0);" class="block text-white hover:text-white mr-4">
+            <a id="sign-out" href="javascript:void(0);" class="block bg-pink-400 rounded-full p-2 text-white hover:bg-pink-500 text-white  mr-4">
                 Sign Out
             </a>
         <?php endif; ?>
